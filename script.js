82b594bd83c2cf9ceaa67b4ff00399e53a77fb0a
Yasmin da Silva <95134741+yaahsilva16@users.noreply.github.com>
2022-01-29 15:18:49 -0300

    entregando exercicio

---
+++ script.js
@@ -0,0 +1,23 @@
+let frutas = ["banana", "uva", "laranja", "melancia", "morango"]
+    console.log(frutas);
+
+let primeira = frutas.unshift('morango')
+     console.log(frutas)
+
+
+let ultimo = frutas.pop()
+     console.log(frutas)
+
+
+let nomes = frutas.push("banana", "uva")
+    console.log(frutas)
+
+ 
+frutas.shift()
+  console.log(frutas)
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+      numbers.sort()
+      console.log(numbers)
+
+    
